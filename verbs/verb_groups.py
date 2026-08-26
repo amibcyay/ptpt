8d8c8647c7f497c8d91b092cabc80a558e50766a
amibcyay <120545303+amibcyay@users.noreply.github.com>
2026-08-26 10:44:23 +0800

    Add files via upload

--- verbs/verb_groups.py
+++ verbs/verb_groups.py
@@ -13,7 +13,8 @@ GROUPS = [
         'estender', 'acrescentar', 'gerar', 'completar', 'variar', 'melhorar', 'limitar', 'encher',
         'alterar', 'diminuir', 'afectar', 'misturar', 'influenciar', 'derivar', 'facilitar', 'reforçar',
         'implicar', 'originar', 'acumular', 'inspirar', 'prejudicar', 'elevar', 'modificar', 'combinar',
-        'beneficiar', 'adaptar',
+        'beneficiar', 'adaptar', 'piorar', 'ajustar', 'amplificar', 'atenuar', 'agravar', 'exacerbar',
+        'intensificar', 'diluir', 'fortalecer', 'enfraquecer', 'concretizar', 'materializar', 'propiciar', 'estimular',
     ]),
     ('Having, needing & ability', [
         'ter', 'poder', 'dever', 'conseguir', 'manter', 'precisar', 'possuir', 'obter',
@@ -23,17 +24,17 @@ GROUPS = [
     ('Wanting, deciding & trying', [
         'querer', 'tentar', 'decidir', 'resolver', 'pretender', 'marcar', 'definir', 'escolher',
         'determinar', 'apontar', 'preparar', 'fixar', 'preferir', 'designar', 'costumar', 'visar',
-        'acertar', 'errar', 'experimentar', 'optar', 'projectar',
+        'acertar', 'errar', 'experimentar', 'optar', 'projectar', 'reconsiderar', 'antecipar',
     ]),
     ('Doing, working & using', [
         'fazer', 'trabalhar', 'usar', 'realizar', 'servir', 'desenvolver', 'utilizar', 'assumir',
         'repetir', 'aplicar', 'funcionar', 'cumprir', 'exercer', 'actuar', 'praticar', 'executar',
-        'cometer', 'agir', 'empregar', 'operar', 'desempenhar', 'efectuar', 'proceder',
+        'cometer', 'agir', 'empregar', 'operar', 'desempenhar', 'efectuar', 'proceder', 'orquestrar',
     ]),
     ('Starting, stopping & continuing', [
         'começar', 'continuar', 'acabar', 'parar', 'iniciar', 'terminar', 'concluir', 'abandonar',
         'deter', 'libertar', 'prosseguir', 'soltar', 'interromper', 'ceder', 'prender', 'retomar',
-        'encerrar', 'largar',
+        'encerrar', 'largar', 'preceder',
     ]),
     ('Going, coming & traveling', [
         'ir', 'passar', 'vir', 'chegar', 'partir', 'sair', 'voltar', 'entrar',
@@ -45,15 +46,16 @@ GROUPS = [
         'levar', 'trazer', 'levantar', 'mandar', 'atingir', 'bater', 'lançar', 'virar',
         'aproximar', 'afastar', 'erguer', 'alcançar', 'cortar', 'enviar', 'baixar', 'atirar',
         'puxar', 'mover', 'carregar', 'arrastar', 'quebrar', 'apertar', 'romper', 'deslocar',
-        'transportar', 'mexer', 'sacudir', 'dobrar', 'derrubar', 'encaminhar',
+        'transportar', 'mexer', 'sacudir', 'dobrar', 'derrubar', 'encaminhar', 'empurrar',
     ]),
     ('Putting, taking & placing', [
         'deixar', 'pôr', 'tomar', 'colocar', 'tirar', 'entregar', 'pegar', 'retirar',
         'meter', 'dispor', 'apanhar', 'recolher', 'arrancar', 'segurar', 'agarrar', 'colher',
+        'remover', 'esvaziar',
     ]),
     ('Opening, closing & covering', [
         'abrir', 'fechar', 'cobrir', 'esconder', 'limpar', 'vestir', 'acender', 'apagar',
-        'lavar',
+        'lavar', 'embrulhar', 'desembrulhar', 'ocultar', 'dissimular',
     ]),
     ('Creating, building & destroying', [
         'criar', 'formar', 'produzir', 'estabelecer', 'construir', 'matar', 'compor', 'fundar',
@@ -61,14 +63,16 @@ GROUPS = [
     ]),
     ('Seeing & looking', [
         'ver', 'apresentar', 'olhar', 'mostrar', 'perceber', 'observar', 'revelar', 'indicar',
-        'destacar', 'notar', 'salientar', 'expor', 'encarar', 'exibir', 'contemplar',
+        'destacar', 'notar', 'salientar', 'expor', 'encarar', 'exibir', 'contemplar', 'iluminar',
+        'obscurecer',
     ]),
     ('Hearing & sensing', [
-        'sentir', 'ouvir', 'sofrer', 'escutar', 'provar', 'tremer',
+        'sentir', 'ouvir', 'sofrer', 'escutar', 'provar', 'tremer', 'cheirar',
     ]),
     ('Body, rest & daily routines', [
         'sentar', 'rir', 'dormir', 'sorrir', 'chorar', 'deitar', 'acordar', 'sonhar',
-        'despertar',
+        'despertar', 'despir', 'calçar', 'pentear', 'barbear', 'banhar', 'secar', 'molhar',
+        'descalçar', 'maquilhar',
     ]),
     ('Eating & drinking', [
         'comer', 'jantar', 'alimentar', 'beber',
@@ -77,39 +81,43 @@ GROUPS = [
         'saber', 'pensar', 'achar', 'conhecer', 'considerar', 'afirmar', 'lembrar', 'entender',
         'acreditar', 'compreender', 'reconhecer', 'prever', 'esquecer', 'imaginar', 'admitir', 'propor',
         'confirmar', 'declarar', 'recordar', 'crer', 'negar', 'sugerir', 'reflectir', 'distinguir',
-        'supor', 'concentrar', 'interpretar', 'confessar', 'alegar',
+        'supor', 'concentrar', 'interpretar', 'confessar', 'alegar', 'duvidar', 'fundamentar', 'postular',
+        'evocar',
     ]),
     ('Learning, reading & writing', [
         'contar', 'escrever', 'ler', 'explicar', 'estudar', 'referir', 'publicar', 'aprender',
         'descrever', 'demonstrar', 'citar', 'ensinar', 'registar', 'assinar', 'registrar', 'esclarecer',
-        'gravar', 'traduzir',
+        'gravar', 'traduzir', 'ilustrar', 'omitir',
     ]),
     ('Speaking & communicating', [
         'dizer', 'falar', 'chamar', 'anunciar', 'gritar', 'informar', 'prometer', 'conversar',
         'calar', 'comentar', 'divulgar', 'transmitir', 'manifestar', 'emitir', 'convencer', 'avisar',
-        'comunicar', 'exclamar', 'murmurar',
+        'comunicar', 'exclamar', 'murmurar', 'mencionar', 'asseverar', 'invocar', 'aludir',
     ]),
     ('Asking, answering & arguing', [
         'pedir', 'perguntar', 'responder', 'aceitar', 'exigir', 'eleger', 'discutir', 'acusar',
         'atender', 'convidar', 'votar', 'recusar', 'justificar', 'insistir', 'concordar', 'criticar',
         'aprovar', 'opor', 'reclamar', 'recorrer', 'denunciar', 'requerer', 'solicitar', 'rejeitar',
-        'convocar',
+        'convocar', 'recomendar', 'discordar', 'opinar', 'argumentar', 'contrapor', 'contrariar', 'desafiar',
+        'questionar',
     ]),
     ('Emotions & attitudes', [
         'gostar', 'desejar', 'aproveitar', 'amar', 'interessar', 'preocupar', 'temer', 'atrair',
-        'surpreender', 'satisfazer', 'ignorar', 'admirar',
+        'surpreender', 'satisfazer', 'ignorar', 'admirar', 'elogiar', 'incentivar',
     ]),
     ('People, help & relationships', [
         'tratar', 'participar', 'ajudar', 'envolver', 'casar', 'apoiar', 'dedicar', 'contribuir',
-        'adoptar', 'cuidar', 'respeitar', 'colaborar',
+        'adoptar', 'cuidar', 'respeitar', 'colaborar', 'cumprimentar', 'relacionar', 'associar', 'resgatar',
+        'hospedar', 'alojar', 'comprometir',
     ]),
     ('Finding, searching & waiting', [
         'encontrar', 'esperar', 'procurar', 'descobrir', 'buscar', 'explorar', 'aguardar', 'investigar',
+        'averiguar', 'indagar', 'perscrutar',
     ]),
     ('Giving, receiving & exchanging', [
         'dar', 'receber', 'incluir', 'oferecer', 'reunir', 'dividir', 'integrar', 'atribuir',
         'substituir', 'prestar', 'ligar', 'fornecer', 'juntar', 'introduzir', 'conceder', 'separar',
-        'unir', 'trocar', 'distribuir', 'espalhar',
+        'unir', 'trocar', 'distribuir', 'espalhar', 'excluir',
     ]),
     ('Money, buying & selling', [
         'perder', 'ganhar', 'pagar', 'vender', 'comprar', 'promover', 'custar', 'importar',
@@ -120,18 +128,19 @@ GROUPS = [
     ]),
     ('Measuring, checking & analyzing', [
         'verificar', 'analisar', 'pesar', 'avaliar', 'comparar', 'calcular', 'medir', 'apurar',
+        'somar', 'multiplicar', 'subtrair',
     ]),
     ('Force, conflict & law', [
         'defender', 'vencer', 'julgar', 'enfrentar', 'conquistar', 'dominar', 'proteger', 'atacar',
         'lutar', 'disputar', 'ameaçar', 'invadir', 'comandar', 'reagir', 'governar', 'condenar',
-        'combater',
+        'combater', 'capturar', 'resguardar',
     ]),
     ('Allowing, forcing & controlling', [
         'permitir', 'garantir', 'evitar', 'obrigar', 'impedir', 'controlar', 'assegurar', 'impor',
-        'proibir',
+        'proibir', 'autorizar', 'suprimir', 'dissuadir', 'persuadir', 'condicionar', 'precaver',
     ]),
     ('Play, arts & leisure', [
         'tocar', 'jogar', 'cantar', 'assistir', 'visitar', 'viajar', 'pintar', 'brincar',
-        'dançar', 'desenhar',
+        'dançar', 'desenhar', 'celebrar', 'festejar', 'comemorar',
     ]),
 ]
